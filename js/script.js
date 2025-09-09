29e086c2ed2fe2d873cef58d0604bbdd56771bed
JDTextilesSA <jdtextilessa@gmail.com>
2025-09-09 10:41:19 -0600

    Primer commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 function imprimirEtiqueta() {
-  const etiqueta = document.querySelector('.etiqueta');
-  const originalBody = document.body.innerHTML;
-
-  // Solo mostrar la etiqueta
-  document.body.innerHTML = etiqueta.outerHTML;
-
-  // Imprimir
+  document.body.classList.add('imprimiendo');
   window.print();
-
-  // Restaurar el contenido original
-  document.body.innerHTML = originalBody;
+  document.body.classList.remove('imprimiendo');
 }
